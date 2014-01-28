64d5c69f6ea46d9e55169c8d6518900679106baa
Anders Tolborg <anders@tolborg.dk>
2014-01-28 13:38:23 +0100

    Change navigation css class

--- theme/bibdk-help-topbar-menu.tpl.php
+++ theme/bibdk-help-topbar-menu.tpl.php
@@ -4,7 +4,7 @@
  * Template file for bibdk_help_topbar_menu
  */
 ?>
-<ul id="user-help-topbar-menu" class="menu horizontal-nav clearfix">
+<ul id="user-help-topbar-menu" class="menu nav--horizontal">
 <?php foreach ($links as $link) { ?>
   <li>
     <?php print l($link['title'], str_replace('node/','overlay/help/',$link['href']), $link); ?>
